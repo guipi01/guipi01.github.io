e52f4630ab4a6b02b9c26552a2045485152e034e
guipi01 <1985859239@qq.com>
2025-04-06 21:20:00 +0800

    Site updated: 2025-04-06 21:19:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/ccc6fa83.html","posts/d36bdd17.html","posts/4a17b156.html","posts/undefined.html","posts/bcd86d8.html","posts/79666db.html","posts/bb16f4e7.html"];function toRandomPost(){
+var posts=["posts/ccc6fa83.html","posts/d36bdd17.html","posts/undefined.html","posts/4a17b156.html","posts/bb16f4e7.html","posts/79666db.html","posts/bcd86d8.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
